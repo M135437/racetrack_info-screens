b35a27017fd1fbd410ac2b9a888a57a2558462f0
olgakuvatova <olga.kuvatova@gmail.com>
2026-03-31 21:31:35 +0300

    backend server running on 3000

---
+++ server/server.js
@@ -0,0 +1,31 @@
+
+// Simple Express server with Socket.IO for real-time communication (minimal setup - module syntax)
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+});
+
+app.get("/test", (req, res) => {
+    res.json({ ok: true });
+});
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+    console.log("client connected:", socket.id);
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
