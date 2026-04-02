9ca0f4c7f883a4eddf4cb7e8f795a6461b2e65f3
Heilika <143094865+Heilika@users.noreply.github.com>
2026-04-02 10:01:21 +0300

    leaderboard + pages setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+import Leaderboard from "./pages/leaderboard/Leaderboard";
+
 function App() {
   const [count, setCount] = useState(0)
 
