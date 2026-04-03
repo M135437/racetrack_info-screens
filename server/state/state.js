6a799a12b3b509ebce295fdd0fddbd44bf5551b9
Mihkel Truup <mihkel.truup@gmail.com>
2026-04-04 00:20:34 +0300

    update state.js - renaming racers to drivers

--- server/state/state.js
+++ server/state/state.js
@@ -8,11 +8,11 @@ let state = {
 		startTime: 1775123702223,		// LocalDateTime // races.race1.startTime
 		hasStarted: true, 	// boolean // races.race1.hasStarted
 		isFinished: false, 	// boolean // races.race1.isFinished
-		racers: [
+		drivers: [
 			{
 			id: 7,		                    // int (incrementing) // racers.racer1.id
 			name: 'näidisvõidusõitja Paul', // string // racers.racer1.name
-			car: 6,		                    // int (1-99) // racers.racer1.car
+			car: 6,		                    // int (1-99) // racers.racer1.car // REVIEW - string võimaldaks nt 'Ferrari Red', 'Yellow Mercedes' 
 			lapCount: 5,                    // int // racers.racer1.lapCount
 			lastLapTimeStamp: 1775123702223, // alguses null, katsetame "2026-04-02T09:55:02.223Z" // racers.racer1.lastLapTimeStamp
 			bestLapTime: 499359,            // alguses null, katsetame racers.racer1.bestLapTime // int millisekundite väljendamiseks
