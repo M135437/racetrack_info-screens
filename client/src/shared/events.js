4edb9f8b985e6e0814701e797404ccc4999105a2
Mihkel Truup <mihkel.truup@gmail.com>
2026-04-05 09:45:40 +0300

    rename events.js race->session part 1

--- client/src/shared/events.js
+++ client/src/shared/events.js
@@ -1,10 +1,14 @@
 const EVENTS = {
     // UI (front-end triggered) actions (requests to create an event, flow from UI to -> back-end)
-    RACE_CREATE: "race:create",     // front desk asks to create race with racers
-    RACE_START: "race:start",       // race control asks to starts race
-    RACE_MODE: "race:mode",         // race control asks to changes flag (state.raceMode.${})
-    RACE_FINISH: "race:finish",     // race control asks to set finish flag (state.raceMode.finish) and finishes race (state.races.${id} -> status:'finished')
-    RACE_END: "race:endSession",    // race control asks to end session 
+    SESSION_CREATE: "session:create",     // front desk asks to create race with racers
+    SESSION_DELETE: "session:delete", // front desk asks for the server to create a session
+    SESSION_GET: "session:get",     // front desk ask for the server for the current list of sessions when the component mounts
+    SESSION_LIST: "session:list",   // front desk ask for listing the updates to the session list (set to listening mode and turn off when component unmounts)
+    SESSION_ERROR: "session:error", //  front desk error handling
+    SESSION_START: "session:start",       // race control asks to starts race
+    SESSION_MODE: "session:mode",         // race control asks to changes flag (state.raceMode.${})
+    SESSION_FINISH: "session:finish",     // race control asks to set finish flag (state.raceMode.finish) and finishes race (state.races.${id} -> status:'finished')
+    SESSION: "session:endSession",    // race control asks to end session 
 
     // back-end recurring timer heartbeat
     TIMER_UPDATE: "timer:update",       // back-end heartbeat sends every timer update (as per config, example every second or every 1/4 of a second)
