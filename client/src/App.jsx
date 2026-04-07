46ee92d69fc4012d13f3e729fc8a1fbc857515f1
mari <marivirkus@gmail.com>
2026-04-07 12:22:47 +0300

    alustan route-imisega

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+
+// npm install react-router-dom (client-kaustas)
+// vajalikud impordid:
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <BrowserRouter> {/* kogu return peab olema mähitud jagajasse */}
     <>
       <section id="center">
         <div className="hero">
@@ -115,7 +117,8 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  </BrowserRouter>
+  );
 }
 
 export default App
