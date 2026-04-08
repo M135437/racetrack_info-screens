5545e13279383fa188831666bcd615cd3841df85
mihkeltruup <mihkeltruup@noreply.localhost>
2026-04-08 06:47:59 +0000

    bugfix on server/state/stateMachine.js import line

--- server/state/stateMachine.js
+++ server/state/stateMachine.js
@@ -1,5 +1,5 @@
 import { ENV_VARIABLES } from "../config/env.js"
-import state from "/server/state/state.js"
+import state from "./state.js"
 
 export function updatePort(RACE_DURATION) {
     state.timer.duration = RACE_DURATION;
