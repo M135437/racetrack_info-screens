543fe873e59f72b34440a081d602b91d91e706a5
mihkeltruup <mihkeltruup@noreply.localhost>
2026-04-08 07:36:25 +0000

    update server/state/stateMachine.js - moving state updates from raceService.js to stateMachine.js

--- server/state/stateMachine.js
+++ server/state/stateMachine.js
@@ -1,7 +1,27 @@
 import { ENV_VARIABLES } from "../config/env.js"
-import state from "/server/state/state.js"
+import state from "./state.js"
 
 export function updatePort(RACE_DURATION) {
     state.timer.duration = RACE_DURATION;
 }
 
+export function stateUptStartSession(session) {
+    state.nextRace = +state.currentRace +1;
+    state.raceMode = RACE_MODES.SAFE;   
+    session.startTime = startTime;  // REVIEW -- vt koos Olgaga, kuidas session mgmt kokku käima saada
+                                    //  - kas läbi state.currentRace.startTime vmuud moodi
+    // endtime - currently out of use // REVIEW
+    session.status = 'started';
+}
+
+export function stateUptChangeMode(mode) {
+    state.raceMode = mode;
+}
+
+export function stateUptFinishMode() {
+    state.raceMode = RACE_MODES.FINISH;
+}
+
+export function stateUptEndSession() {
+    state.raceMode = RACE_MODES.ENDED;
+}
