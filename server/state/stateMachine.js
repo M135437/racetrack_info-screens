29de21f0d829073d4798068c47d3a1943685d9e5
Mihkel Truup <mihkel.truup@gmail.com>
2026-04-09 16:44:23 +0300

    update stateMachine.js and plan function getNextPendingRaceId

--- server/state/stateMachine.js
+++ server/state/stateMachine.js
@@ -1,27 +1,35 @@
 import { ENV_VARIABLES } from "../config/env.js"
 import state from "./state.js"
+import { RACE_MODES } from "../../client/src/shared/types.js"
 
 export function updatePort(RACE_DURATION) {
     state.timer.duration = RACE_DURATION;
 }
 
 export function stateUptStartSession(session) {
-    state.nextRace = +state.currentRace +1;
-    state.raceMode = RACE_MODES.SAFE;   
-    session.startTime = startTime;  // REVIEW -- vt koos Olgaga, kuidas session mgmt kokku käima saada
-                                    //  - kas läbi state.currentRace.startTime vmuud moodi
-    // endtime - currently out of use // REVIEW
+    if (!session) {
+        return;
+    }
+    state.runningRace = session.id;
+    state.raceMode = RACE_MODES.SAFE;
+    session.startTime = Date.now();
+    session.startTimeStamp = Date.now();
+    session.hasStarted = true;
     session.status = 'started';
+    // state.nextRace = getNextPendingRaceId(); // eraldi funktsiooni jagu ülesanne
 }
 
 export function stateUptChangeMode(mode) {
     state.raceMode = mode;
 }
 
-export function stateUptFinishMode() {
+export function stateUptFinishMode(mode) {
     state.raceMode = RACE_MODES.FINISH;
 }
 
 export function stateUptEndSession() {
     state.raceMode = RACE_MODES.ENDED;
+    state.runningRace = null;
+    state.currentRace = null;
+    state.nextRace = getNextPendingRaceId();
 }
