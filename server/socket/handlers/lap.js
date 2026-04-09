55c30e9b3c5829d5368b4d7ba60407ac86927628
mari <marivirkus@gmail.com>
2026-04-09 12:21:26 +0300

    handleris racer->driver pluss mock-käivituse eemaldamine

--- server/socket/handlers/lap.js
+++ server/socket/handlers/lap.js
@@ -7,9 +7,9 @@ TOIMUS MINGISUGUNE MUUDATUS
 nn switchboard-logic -> saab signaali ja suunab töötlusse */
 
 // IMPORDID (testandmetega):
-import { recordLap, getMockState, startMockRace } from "../../services/lapService.js";
+import { recordLap/*, getMockState, startMockRace */ } from "../../services/lapService.js";
 // seisund - aeg (taimer) ja objekt (racer)
-// import { state } from "../../state/state.js"; <- selgus, et handleris
+// import { state } from "../../state/state.js";
 // ebavajalik, sest recordLap ise oma failis impordib ja kasutab
 // infovahetus
 // import { emitState } from "../index.js";
@@ -17,9 +17,7 @@ import { recordLap, getMockState, startMockRace } from "../../services/lapServic
 
 // event-ide import ühtlustamaks io-socket infovahetust:
 import EVENTS from "../../../client/src/shared/events.js";
-/* pärast dev2 muutusi, edaspidi tuleb events.js kaudu emit-on suhtlus:
-import EVENTS from "../../../client/src/shared/events.js"
-st:
+/* pärast dev2 muutusi, edaspidi tuleb events.js kaudu emit-on suhtlus, st:
 race:started -> EVENTS.SESSION_STARTED
 record-lap -> EVENTS.LAP_RECORD
 lap:updated -> EVENTS.LAP_RECORDED
@@ -39,35 +37,38 @@ export const lapHandler = (io, socket) => {
     });
 }; */
 
-// NB! ilmselt tuleb muudatus, kus:
+// NB! TEHTUD MUUDATUS, kus:
 // võistlus ise -> session
 // racer -> driver
 
-// MOCK-andmetega testversioon handler-ist:
+// (testfaasis oli MOCK-andmetega testversioon handler-ist)
 export default (io, socket) => {
 
     // ühendusprobleemi kontroll:
     console.log("LAP HANDLER TÖÖTAB SOCKETIGA:", socket.id);
 
+    /* TESTFAASIS käiviti (sandbox-server vers):
     // kui LapTracker komponent laeb, saadetakse sellele mock-andmete info
-    socket.emit("lap:init", getMockState().racers);
+    socket.emit("lap:init", getMockState().drivers); */
 
+    /* TESTFAASIS viitega sõidualustamine:
     // automaatne võidusõidu alustamine, et testida, kas nupud
     // tekivad alles siis kui on vajutatud "start"
     setTimeout(() => {
         startMockRace(); //seame vajalikud muundujad paika
         io.emit(EVENTS.SESSION_STARTED, getMockState());
     }, 3000); // viide 3 sek; siis tekivad nupud. enne seda "waiting.."
+    */
 
     // nupuvajutusel:
-    socket.on(EVENTS.LAP_UPDATE, (racerId) => {
-        const updatedRacer = recordLap(racerId);
+    socket.on(EVENTS.LAP_UPDATE, (driverId) => {
+        const updatedDriver = recordLap(driverId);
 
-        if (updatedRacer) {
-            // pole veel ühtset state-emit-i, seega tavaline io:
-            io.emit(EVENTS.LAP_UPDATED, updatedRacer);
+        if (updatedDriver) {
+            // pole (veel?) ühtset state-emit-i, seega tavaline io:
+            io.emit(EVENTS.LAP_UPDATED, updatedDriver);
             // ja testi jaoks jälle nupuvajutusel kontrolltekst:
-            console.log(`Car ${updatedRacer.car} crossed line: ${updatedRacer.latestLapTime}s`);
+            console.log(`Car ${updatedDriver.car} crossed line: ${updatedDriver.latestLapTime}s`);
         }
     })
 }
