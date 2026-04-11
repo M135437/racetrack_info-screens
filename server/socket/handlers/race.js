504011dfaccb13e09ea8828b7a99ebcfd2d0be51
Mihkel Truup <mihkel.truup@gmail.com>
2026-04-11 16:59:26 +0300

    bugfix server/socket/handlers/race.js was on wrong socket - (socket, io) swapped with (io, socket)

--- server/socket/handlers/race.js
+++ server/socket/handlers/race.js
@@ -1,8 +1,13 @@
 import EVENTS from "../../../client/src/shared/events.js"
 import raceService from "../../services/raceService.js"
 
-export default function raceHandler (socket, io) {
+export default function raceHandler (io, socket) {
+    console.log("raceHandler attached");
+    socket.onAny((event, ...args) => {
+        console.log("EVENT RECEIVED:", event, args);
+    });
     socket.on(EVENTS.SESSION_START, () => {
+        console.log("START RECEIVED");
         raceService.startSession(io);
     });
     socket.on(EVENTS.SESSION_MODE, (mode) => {
