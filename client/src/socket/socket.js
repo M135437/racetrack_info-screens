a9107cdf4c904e700a455576962071689a20598f
mihkeltruup <mihkeltruup@noreply.localhost>
2026-04-12 06:26:19 +0000

    resolving merge conflict on client/src/socket/socket.js - no technical changes

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-// Ühendus backendiga (port 3000)
+// connection to backend (assuming port 3000)
 export const socket = io("http://localhost:3000", {
     autoConnect: true
 });
