c95f8d3aaaa152b36a118bdfa87768be50245b5e
mihkeltruup <mihkeltruup@noreply.localhost>
2026-04-12 06:06:54 +0000

    Update server/state/state.js

--- server/state/state.js
+++ server/state/state.js
@@ -2,11 +2,11 @@ let state = {
 	sessions: [
 	 	{
 		id: 9,		// int  // race.id
-		status: 'started', 	// string "notStarted", "started", "finishing", "ended" // races.race1.status // NB! duplicate against hasStarted&isFinished
-							// preferred if you use this value and set up hasStarted&isFinished locally
-		startTimeStamp: 1775123702223,	// LocalDateTime // races.race9.startTimeStamp
-		secondsLeft: 100641 ,	// int milliseconds // races.race1.secondsLeft  // REVIEW @MARI - secondsLeft --> msLeft? OK? NOT_OK?
-		startTime: 1775123702223,		// LocalDateTime // races.race1.startTime
+		name: "FailistTulevSessioon",
+		status: 'notStarted', 	// string "upcoming", "started", "finishing", "ended" // races.race1.status // NB! duplicate against hasStarted&isFinished
+		startTimeStamp: 1775123702223,	// LocalDateTime // races.race9.startTimeStamp	// is this redundant? let's just use state.timer.startTime?
+		secondsLeft: 100641 ,	// int milliseconds // races.race1.secondsLeft  // REVIEW @MARI - secondsLeft --> msLeft? // is this redundant?
+		startTime: 1775123702223,		// LocalDateTime // races.race1.startTime // is this redundant? startTimeStamp or startTime?
 		hasStarted: true, 	// boolean // races.race1.hasStarted
 		isFinished: false, 	// boolean // races.race1.isFinished
 		drivers: [
@@ -20,23 +20,23 @@ let state = {
 			isFinished: false               // boolean // racers.racer1.isFinished
 			}
 			// ... kuni racer8
-		],  // racers massiivi lõpp
+		],  // drivers massiivi lõpp
 		} 	// ... kuni indefinited race#
-		],	// races massiivi lõpp
+		],	// sessions massiivi lõpp
 	
 	runningRace: null, 	// initially null, upon start or equals(race.id) -> use as pointer to state.races.{}
-	nextRace: 0,	// initially 0, increments to 1 as soon as session/race 0 starts
+	nextRace: 0,	// initially 0, increments to next session id as soon as session/race starts
     raceMode: 'notStarted', 	// initially 'notStarted', options 'safe', 'hazard', 'danger', 'finishing', 'ended'
     session: 'started', //'null', 'started' or 'ended' once 10mins/1min runs out
     timer: {
-        duration: 6000000,              // 600 000 ms = 10 min, dev 60 000 = 1 min // set by env var DEV MODE
-        timeRemaining: 100641,          // ms value for heartbeat
-        startTime: 1775123702223,       // LocalDateTime -- when was session started
+        duration: null,              // 600 000 ms = 10 min, dev 60 000 = 1 min // set by env var DEV MODE
+        timeRemaining: null,          // ms value for heartbeat
+        startTime: null,       // LocalDateTime -- when was session started
         endTime: null,                  // LocalDateTime -- when does session run out of time (REVIEW - non-mandatory)
         timerStatus: null
     },
 
-    leaderboard: [], // server updates this with new driver info + sorted by bestLapTime each time socket.emit(EVENTS.LAP_UPDATED) is ran
+    leaderboard: [],
 	};
 
 export default state;
