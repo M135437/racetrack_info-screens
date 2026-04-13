522f4bd697b4c128c9cf82c17267f620d5f6b4d8
Heilika <143094865+Heilika@users.noreply.github.com>
2026-04-13 07:44:21 +0300

    fix: remove vite default styles, use window.location.origin for socket

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 
-// connection to backend (assuming port 3000)
-export const socket = io("http://localhost:3000", {
+export const socket = io(window.location.origin, {
     autoConnect: true
 });
