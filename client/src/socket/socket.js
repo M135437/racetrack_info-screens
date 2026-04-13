11ce967659233c8e9f70211f7e78c4b33e701112
mihkeltruup <mihkeltruup@noreply.localhost>
2026-04-13 14:01:02 +0000

    update client/src/socket/socket.js with window.location.protocol + default port to 3000

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000",{
+const PORT = import.meta.env.VITE_SOCKET_PORT || "3000";
+
+// SOCKET_URL automatic adjustment to window.location.protocol (localhost / LAN / prod domain)
+const SOCKET_URL = `${window.location.protocol}//${window.location.hostname}:${PORT}`;
+
+export const socket = io(SOCKET_URL, {
     autoConnect: true
 });
