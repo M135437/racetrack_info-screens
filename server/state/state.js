ba472bf606bf18b31befc80c75735fdc4a41d2f6
olgakuvatova <olga.kuvatova@gmail.com>
2026-04-15 11:52:06 +0300

    We killed Paul

--- server/state/state.js
+++ server/state/state.js
@@ -1,42 +1,19 @@
 let state = {
-	sessions: [
-	 	{
-		id: 9,		// int  // race.id
-		name: "FailistTulevSessioon",
-		status: 'notStarted', 	// string "upcoming", "started", "finishing", "ended" // races.race1.status // NB! duplicate against hasStarted&isFinished
-		startTimeStamp: 1775123702223,	// LocalDateTime // races.race9.startTimeStamp	// is this redundant? let's just use state.timer.startTime?
-		secondsLeft: 100641 ,	// int milliseconds // races.race1.secondsLeft  // REVIEW @MARI - secondsLeft --> msLeft? // is this redundant?
-		startTime: 1775123702223,		// LocalDateTime // races.race1.startTime // is this redundant? startTimeStamp or startTime?
-		hasStarted: true, 	// boolean // races.race1.hasStarted
-		isFinished: false, 	// boolean // races.race1.isFinished
-		drivers: [
-			{
-			id: 7,		                    // int (incrementing) // racers.racer1.id
-			name: 'näidisvõidusõitja Paul', // string // racers.racer1.name
-			car: 6,		                    // int (1-99) // racers.racer1.car // REVIEW - string võimaldaks nt 'Ferrari Red', 'Yellow Mercedes' 
-			lapCount: 5,                    // int // racers.racer1.lapCount
-			lastLapTimeStamp: 1775123702223, // alguses null, katsetame "2026-04-02T09:55:02.223Z" // racers.racer1.lastLapTimeStamp
-			bestLapTime: 499359,            // alguses null, katsetame racers.racer1.bestLapTime // int millisekundite väljendamiseks
-			isFinished: false               // boolean // racers.racer1.isFinished
-			}
-			// ... kuni racer8
-		],  // drivers massiivi lõpp
-		} 	// ... kuni indefinited race#
-		],	// sessions massiivi lõpp
-	
+	sessions: [],	// sessions massiivi lõpp
+
 	runningRace: null, 	// initially null, upon start or equals(race.id) -> use as pointer to state.races.{}
 	nextRace: 0,	// initially 0, increments to next session id as soon as session/race starts
-    raceMode: 'notStarted', 	// initially 'notStarted', options 'safe', 'hazard', 'danger', 'finishing', 'ended'
-    session: 'started', //'null', 'started' or 'ended' once 10mins/1min runs out
-    timer: {
-        duration: null,              // 600 000 ms = 10 min, dev 60 000 = 1 min // set by env var DEV MODE
-        timeRemaining: null,          // ms value for heartbeat
-        startTime: null,       // LocalDateTime -- when was session started
-        endTime: null,                  // LocalDateTime -- when does session run out of time (REVIEW - non-mandatory)
-        timerStatus: null
-    },
+	raceMode: 'notStarted', 	// initially 'notStarted', options 'safe', 'hazard', 'danger', 'finishing', 'ended'
+	session: 'started', //'null', 'started' or 'ended' once 10mins/1min runs out
+	timer: {
+		duration: null,              // 600 000 ms = 10 min, dev 60 000 = 1 min // set by env var DEV MODE
+		timeRemaining: null,          // ms value for heartbeat
+		startTime: null,       // LocalDateTime -- when was session started
+		endTime: null,                  // LocalDateTime -- when does session run out of time (REVIEW - non-mandatory)
+		timerStatus: null
+	},
 
-    leaderboard: [],
-	};
+	leaderboard: [],
+};
 
 export default state;
