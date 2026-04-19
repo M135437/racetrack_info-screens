aaf0b7d6269cff9e452b2acbd49539825d79e1a2
olgakuvatova <olga.kuvatova@gmail.com>
2026-04-19 09:50:47 +0300

    dev and prod modes now start with correct amount of minutes (1 min/10 min)

--- server/server.js
+++ server/server.js
@@ -8,10 +8,13 @@ import { ENV_VARIABLES, RACE_DURATION } from "./config/env.js"
 import { setDuration } from "./state/stateMachine.js"
 import { loadState } from "./utils/persistState.js"
 
+import state from "./state/state.js" //Olga - for testing purposes, to check if state is loaded correctly
+
 // check that env variables are set to control if application can be started,
 // set race duration accordingly
-processEnvVar()
-setDuration(RACE_DURATION);
+processEnvVar() // check env variables and set defaults if not set, also set duration from env variable if not already set in state (FIX) Olga
+//setDuration(RACE_DURATION); //Olga
+
 
 // Simple Express server with Socket.IO for real-time communication (minimal setup - module syntax)
 const app = express();
@@ -31,6 +34,35 @@ const io = new Server(server, {
     cors: { origin: "*" }
 });
 
+//Async bootstrap function to initialize the server and load state before starting the server (FIX) Olga
+async function startServer() {
+    await loadState(); // Load state before starting the server
+
+    const hasSavedDuration = state?.timer?.duration
+    // Set duration from env variable if not already set in state, otherwise keep the loaded duration
+    if (!state.timer || !state.timer.duration) {
+        setDuration(RACE_DURATION)
+    }
+
+    /* if (hasSavedDuration) {
+        setDuration(RACE_DURATION); // Set duration from env variable if not already set in state
+    }*/
+
+    //SOCKETS
+
+    socketHandlers(io);
+
+    //START SERVER
+    server.listen(ENV_VARIABLES.RACETRACK_SERVER_PORT, () => {
+        console.log(`Server running on port ${ENV_VARIABLES.RACETRACK_SERVER_PORT}`)
+        console.log(`Race duration: ${(state.timer.duration / 1000) / 60} min`)
+    })
+}
+
+startServer();
+
+
+/*
 // pass Socket.IO on to handlers
 socketHandlers(io);
 
@@ -40,6 +72,6 @@ console.log(loadState());
 server.listen(ENV_VARIABLES.RACETRACK_SERVER_PORT, () => {
     console.log(`  ➜  Server running on port ${ENV_VARIABLES.RACETRACK_SERVER_PORT}\n  ➜  http://localhost:${ENV_VARIABLES.RACETRACK_SERVER_PORT}/`);
     console.log(`  ➜  Race duration set to ${(RACE_DURATION / 1000) / 60} minutes`);
-    
-});
+
+});*/
 
